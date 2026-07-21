1acf8551337ddec1e13262339ba292da31305bbe
JaviER-CdelU <JAVIERGRATTAROLA@GMAIL.COM>
2026-07-21 04:29:29 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "salvame-papas-v16-firebase-cliente";
+const CACHE_NAME = "salvame-papas-v17-comercios-reales";
 const APP_SHELL = ["./","./index.html","./comercio.html","./admin.html","./seguimiento-comercios.html","./como-usar.html","./diccionario-entrerriano.html","./offline.html","./manifest.webmanifest"];
 
 self.addEventListener("install",event=>{
