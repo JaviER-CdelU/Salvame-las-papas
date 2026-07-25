33f852222bfb66edbe0233a4d095171481ca0941
JaviER-CdelU <JAVIERGRATTAROLA@GMAIL.COM>
2026-07-25 04:55:23 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "salvame-papas-v54-ofertas-carrito";
+const CACHE_NAME = "salvame-papas-v55-compartir-comercios";
 const APP_SHELL = ["./","./index.html","./comercio.html","./admin.html","./seguimiento-comercios.html","./como-usar.html","./diccionario-entrerriano.html","./offline.html","./restablecer-clave.html","./manifest.webmanifest","./version.json"];
 
 self.addEventListener("install",event=>{
