cd9d9041829731e4c2a56bb0766617643db4d9d8
JaviER-CdelU <JAVIERGRATTAROLA@GMAIL.COM>
2026-07-30 03:32:36 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,2 @@
-self.addEventListener("install", event => { self.skipWaiting(); });
-self.addEventListener("activate", event => {
-  event.waitUntil((async () => {
-    const names = await caches.keys();
-    await Promise.all(names.map(name => caches.delete(name)));
-    await self.registration.unregister();
-    const clients = await self.clients.matchAll({ type: "window" });
-    clients.forEach(client => client.navigate(client.url));
-  })());
-});
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()));
